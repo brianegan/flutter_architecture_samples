a9586e845a34fe6f3908c6e27650c3713a68e509
Brian Egan <brian@brianegan.com>
2017-11-03 15:43:32 +0100

    Add needed generated models

---
+++ example/built_redux/lib/models/app_state.g.dart
@@ -0,0 +1,196 @@
+// GENERATED CODE - DO NOT MODIFY BY HAND
+
+part of app_state;
+
+// **************************************************************************
+// Generator: BuiltValueGenerator
+// **************************************************************************
+
+// ignore_for_file: always_put_control_body_on_new_line
+// ignore_for_file: annotate_overrides
+// ignore_for_file: avoid_annotating_with_dynamic
+// ignore_for_file: avoid_returning_this
+// ignore_for_file: omit_local_variable_types
+// ignore_for_file: prefer_expression_function_bodies
+// ignore_for_file: sort_constructors_first
+
+Serializer<AppState> _$appStateSerializer = new _$AppStateSerializer();
+
+class _$AppStateSerializer implements StructuredSerializer<AppState> {
+  @override
+  final Iterable<Type> types = const [AppState, _$AppState];
+  @override
+  final String wireName = 'AppState';
+
+  @override
+  Iterable serialize(Serializers serializers, AppState object,
+      {FullType specifiedType: FullType.unspecified}) {
+    final result = <Object>[
+      'isLoading',
+      serializers.serialize(object.isLoading,
+          specifiedType: const FullType(bool)),
+      'todos',
+      serializers.serialize(object.todos,
+          specifiedType:
+              const FullType(BuiltList, const [const FullType(Todo)])),
+      'activeTab',
+      serializers.serialize(object.activeTab,
+          specifiedType: const FullType(AppTab)),
+      'activeFilter',
+      serializers.serialize(object.activeFilter,
+          specifiedType: const FullType(VisibilityFilter)),
+    ];
+
+    return result;
+  }
+
+  @override
+  AppState deserialize(Serializers serializers, Iterable serialized,
+      {FullType specifiedType: FullType.unspecified}) {
+    final result = new AppStateBuilder();
+
+    final iterator = serialized.iterator;
+    while (iterator.moveNext()) {
+      final key = iterator.current as String;
+      iterator.moveNext();
+      final dynamic value = iterator.current;
+      switch (key) {
+        case 'isLoading':
+          result.isLoading = serializers.deserialize(value,
+              specifiedType: const FullType(bool)) as bool;
+          break;
+        case 'todos':
+          result.todos.replace(serializers.deserialize(value,
+                  specifiedType:
+                      const FullType(BuiltList, const [const FullType(Todo)]))
+              as BuiltList<Todo>);
+          break;
+        case 'activeTab':
+          result.activeTab = serializers.deserialize(value,
+              specifiedType: const FullType(AppTab)) as AppTab;
+          break;
+        case 'activeFilter':
+          result.activeFilter = serializers.deserialize(value,
+                  specifiedType: const FullType(VisibilityFilter))
+              as VisibilityFilter;
+          break;
+      }
+    }
+
+    return result.build();
+  }
+}
+
+class _$AppState extends AppState {
+  @override
+  final bool isLoading;
+  @override
+  final BuiltList<Todo> todos;
+  @override
+  final AppTab activeTab;
+  @override
+  final VisibilityFilter activeFilter;
+
+  factory _$AppState([void updates(AppStateBuilder b)]) =>
+      (new AppStateBuilder()..update(updates)).build();
+
+  _$AppState._({this.isLoading, this.todos, this.activeTab, this.activeFilter})
+      : super._() {
+    if (isLoading == null) throw new ArgumentError.notNull('isLoading');
+    if (todos == null) throw new ArgumentError.notNull('todos');
+    if (activeTab == null) throw new ArgumentError.notNull('activeTab');
+    if (activeFilter == null) throw new ArgumentError.notNull('activeFilter');
+  }
+
+  @override
+  AppState rebuild(void updates(AppStateBuilder b)) =>
+      (toBuilder()..update(updates)).build();
+
+  @override
+  AppStateBuilder toBuilder() => new AppStateBuilder()..replace(this);
+
+  @override
+  bool operator ==(dynamic other) {
+    if (identical(other, this)) return true;
+    if (other is! AppState) return false;
+    return isLoading == other.isLoading &&
+        todos == other.todos &&
+        activeTab == other.activeTab &&
+        activeFilter == other.activeFilter;
+  }
+
+  @override
+  int get hashCode {
+    return $jf($jc(
+        $jc($jc($jc(0, isLoading.hashCode), todos.hashCode),
+            activeTab.hashCode),
+        activeFilter.hashCode));
+  }
+
+  @override
+  String toString() {
+    return (newBuiltValueToStringHelper('AppState')
+          ..add('isLoading', isLoading)
+          ..add('todos', todos)
+          ..add('activeTab', activeTab)
+          ..add('activeFilter', activeFilter))
+        .toString();
+  }
+}
+
+class AppStateBuilder implements Builder<AppState, AppStateBuilder> {
+  _$AppState _$v;
+
+  bool _isLoading;
+  bool get isLoading => _$this._isLoading;
+  set isLoading(bool isLoading) => _$this._isLoading = isLoading;
+
+  ListBuilder<Todo> _todos;
+  ListBuilder<Todo> get todos => _$this._todos ??= new ListBuilder<Todo>();
+  set todos(ListBuilder<Todo> todos) => _$this._todos = todos;
+
+  AppTab _activeTab;
+  AppTab get activeTab => _$this._activeTab;
+  set activeTab(AppTab activeTab) => _$this._activeTab = activeTab;
+
+  VisibilityFilter _activeFilter;
+  VisibilityFilter get activeFilter => _$this._activeFilter;
+  set activeFilter(VisibilityFilter activeFilter) =>
+      _$this._activeFilter = activeFilter;
+
+  AppStateBuilder();
+
+  AppStateBuilder get _$this {
+    if (_$v != null) {
+      _isLoading = _$v.isLoading;
+      _todos = _$v.todos?.toBuilder();
+      _activeTab = _$v.activeTab;
+      _activeFilter = _$v.activeFilter;
+      _$v = null;
+    }
+    return this;
+  }
+
+  @override
+  void replace(AppState other) {
+    if (other == null) throw new ArgumentError.notNull('other');
+    _$v = other as _$AppState;
+  }
+
+  @override
+  void update(void updates(AppStateBuilder b)) {
+    if (updates != null) updates(this);
+  }
+
+  @override
+  _$AppState build() {
+    final _$result = _$v ??
+        new _$AppState._(
+            isLoading: isLoading,
+            todos: todos?.build(),
+            activeTab: activeTab,
+            activeFilter: activeFilter);
+    replace(_$result);
+    return _$result;
+  }
+}
---
+++ example/built_redux/lib/models/extra_actions.g.dart
@@ -0,0 +1,55 @@
+// GENERATED CODE - DO NOT MODIFY BY HAND
+
+part of extra_actions;
+
+// **************************************************************************
+// Generator: BuiltValueGenerator
+// **************************************************************************
+
+// ignore_for_file: always_put_control_body_on_new_line
+// ignore_for_file: annotate_overrides
+// ignore_for_file: avoid_annotating_with_dynamic
+// ignore_for_file: avoid_returning_this
+// ignore_for_file: omit_local_variable_types
+// ignore_for_file: prefer_expression_function_bodies
+// ignore_for_file: sort_constructors_first
+
+const ExtraAction _$toggleAllComplete =
+    const ExtraAction._('toggleAllComplete');
+const ExtraAction _$clearCompleted = const ExtraAction._('clearCompleted');
+
+ExtraAction _$extraActionValueOf(String name) {
+  switch (name) {
+    case 'toggleAllComplete':
+      return _$toggleAllComplete;
+    case 'clearCompleted':
+      return _$clearCompleted;
+    default:
+      throw new ArgumentError(name);
+  }
+}
+
+final BuiltSet<ExtraAction> _$extraActionValues =
+    new BuiltSet<ExtraAction>(const <ExtraAction>[
+  _$toggleAllComplete,
+  _$clearCompleted,
+]);
+
+Serializer<ExtraAction> _$extraActionSerializer = new _$ExtraActionSerializer();
+
+class _$ExtraActionSerializer implements PrimitiveSerializer<ExtraAction> {
+  @override
+  final Iterable<Type> types = const <Type>[ExtraAction];
+  @override
+  final String wireName = 'ExtraAction';
+
+  @override
+  Object serialize(Serializers serializers, ExtraAction object,
+          {FullType specifiedType: FullType.unspecified}) =>
+      object.name;
+
+  @override
+  ExtraAction deserialize(Serializers serializers, Object serialized,
+          {FullType specifiedType: FullType.unspecified}) =>
+      ExtraAction.valueOf(serialized as String);
+}
