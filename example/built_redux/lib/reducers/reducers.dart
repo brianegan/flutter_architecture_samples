d69fba02d3dd5c9cedaeda88efb91eea74e94a18
David Marne <davemarne@gmail.com>
2017-11-04 18:18:19 -0600

    built_redux_tweaks - move selector to the app state object to take advantage of memoized getters

--- example/built_redux/lib/reducers/reducers.dart
+++ example/built_redux/lib/reducers/reducers.dart
@@ -1,8 +1,6 @@
-import 'package:built_collection/built_collection.dart';
 import 'package:built_redux/built_redux.dart';
 import 'package:built_redux_sample/models/models.dart';
 import 'package:built_redux_sample/actions/actions.dart';
-import 'package:built_redux_sample/selectors/selectors.dart';
 
 var reducerBuilder = new ReducerBuilder<AppState, AppStateBuilder>()
   ..add(AppActionsNames.addTodoAction, addTodo)
@@ -15,52 +13,50 @@ var reducerBuilder = new ReducerBuilder<AppState, AppStateBuilder>()
   ..add(AppActionsNames.loadTodosSuccess, todosLoaded)
   ..add(AppActionsNames.loadTodosFailure, todosLoadFailed);
 
-addTodo(AppState state, Action<Todo> action, AppStateBuilder builder) =>
-    builder.todos = state.todos.toBuilder()..add(action.payload);
+addTodo(AppState state, Action<Todo> action, AppStateBuilder builder) {
+  builder.todos.add(action.payload);
+}
 
-clearCompleted(AppState state, Action<Null> action, AppStateBuilder builder) =>
-    builder.todos = state.todos.toBuilder()..where((todo) => !todo.complete);
+clearCompleted(AppState state, Action<Null> action, AppStateBuilder builder) {
+  builder.todos.where((todo) => !todo.complete);
+}
 
-deleteTodo(AppState state, Action<String> action, AppStateBuilder builder) =>
-    builder.todos = state.todos.toBuilder()
-      ..where((todo) => todo.id != action.payload);
+deleteTodo(AppState state, Action<String> action, AppStateBuilder builder) {
+  builder.todos.where((todo) => todo.id != action.payload);
+}
 
 toggleAll(AppState state, Action<Null> action, AppStateBuilder builder) {
-  final allComplete = allCompleteSelector(todosSelector(state));
+  final allComplete = state.allCompleteSelector;
 
   return builder.todos = state.todos.toBuilder()
     ..map((todo) => (todo.toBuilder()..complete = !allComplete).build());
 }
 
 updateFilter(
-  AppState state,
-  Action<VisibilityFilter> action,
-  AppStateBuilder builder,
-) =>
-    builder.activeFilter = action.payload;
+    AppState state, Action<VisibilityFilter> action, AppStateBuilder builder) {
+  builder.activeFilter = action.payload;
+}
 
-updateTab(AppState state, Action<AppTab> action, AppStateBuilder builder) =>
-    builder.activeTab = action.payload;
+updateTab(AppState state, Action<AppTab> action, AppStateBuilder builder) {
+  builder.activeTab = action.payload;
+}
 
 todosLoaded(
     AppState state, Action<List<Todo>> action, AppStateBuilder builder) {
   builder
     ..isLoading = false
-    ..todos = new ListBuilder<Todo>(action.payload);
+    ..todos.addAll(action.payload);
 }
 
 todosLoadFailed(
     AppState state, Action<Object> action, AppStateBuilder builder) {
   builder
     ..isLoading = false
-    ..todos = new ListBuilder<Todo>([]);
+    ..todos.clear();
 }
 
-updateTodo(
-  AppState state,
-  Action<UpdateTodoActionPayload> action,
-  AppStateBuilder builder,
-) =>
-    builder.todos = state.todos.toBuilder()
-      ..map((todo) =>
-          todo.id == action.payload.id ? action.payload.updatedTodo : todo);
+updateTodo(AppState state, Action<UpdateTodoActionPayload> action,
+    AppStateBuilder builder) {
+  builder.todos.map((todo) =>
+      todo.id == action.payload.id ? action.payload.updatedTodo : todo);
+}
