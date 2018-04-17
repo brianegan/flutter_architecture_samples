70f523800ef7b268a08fb6b585b0de5be6c71d87
Brian Egan <brian@brianegan.com>
2018-04-17 21:14:06 +0200

    WIP

---
+++ example/mvi_base/lib/mvi_base.dart
@@ -0,0 +1,11 @@
+// Copyright 2018 The Flutter Architecture Sample Authors. All rights reserved.
+// Use of this source code is governed by the MIT license that can be found
+// in the LICENSE file.
+
+library blocs;
+
+export 'src/todos_list_bloc.dart';
+export 'src/todo_bloc.dart';
+export 'src/user_bloc.dart';
+export 'src/stats_bloc.dart';
+export 'src/models/models.dart';
---
+++ example/mvi_base/test/todo_bloc_test.dart
@@ -0,0 +1,73 @@
+// Copyright 2018 The Flutter Architecture Sample Authors. All rights reserved.
+// Use of this source code is governed by the MIT license that can be found
+// in the LICENSE file.
+
+import 'dart:async';
+
+import 'package:mvi_base/mvi_base.dart';
+import 'package:mvi_base/src/models/models.dart';
+import 'package:mockito/mockito.dart';
+import 'package:rxdart/rxdart.dart';
+import 'package:test/test.dart';
+import 'package:todos_repository/todos_repository.dart';
+
+class MockReactiveTodosRepository extends Mock
+    implements ReactiveTodosRepository {}
+
+void main() {
+  group('TodoBloc', () {
+    test('should load a todo from the Repo', () {
+      final repository = new MockReactiveTodosRepository();
+      final bloc = new TodoBloc(repository);
+      final entity = new TodoEntity("Hallo", "1", "Note", false);
+      final todos = [entity];
+      final source = new BehaviorSubject<List<TodoEntity>>(seedValue: todos);
+
+      when(repository.todos()).thenReturn(source.stream);
+
+      expect(bloc.todo("1"), emits(Todo.fromEntity(entity)));
+    });
+
+    test('emits null if the todo is not present', () {
+      final repository = new MockReactiveTodosRepository();
+      final bloc = new TodoBloc(repository);
+      final entity = new TodoEntity("Hallo", "1", "Note", false);
+      final todos = [entity];
+      final source = new BehaviorSubject<List<TodoEntity>>(seedValue: todos);
+
+      when(repository.todos()).thenReturn(source.stream);
+
+      expect(bloc.todo("2"), emits(null));
+    });
+
+
+
+    test('should send deletions to the repo', () async {
+      final repository = new MockReactiveTodosRepository();
+      final source = new BehaviorSubject<List<TodoEntity>>();
+
+      when(repository.todos()).thenReturn(source.stream);
+      when(repository.deleteTodo(["1"])).thenReturn(new Future.value());
+
+      final bloc = new TodoBloc(repository);
+      bloc.deleteTodo.add("1");
+
+      verify(repository.deleteTodo(["1"]));
+    });
+
+    test('should send updates to the repo', () async {
+      final repository = new MockReactiveTodosRepository();
+      final update = new Todo('Waaaat');
+      final source = new BehaviorSubject<List<TodoEntity>>();
+
+      when(repository.todos()).thenReturn(source.stream);
+      when(repository.updateTodo(update.toEntity()))
+          .thenReturn(new Future.value());
+
+      final bloc = new TodoBloc(repository);
+      bloc.updateTodo.add(update);
+
+      verify(repository.updateTodo(update.toEntity()));
+    });
+  });
+}
