0eb257bae01549e2c902a208311c96f46b0f24be
Brian Egan <brian@brianegan.com>
2018-09-20 23:36:24 +0200

    Fixes #55: Disable filter buttons when fading out

--- example/mvi_flutter/lib/widgets/filter_button.dart
+++ example/mvi_flutter/lib/widgets/filter_button.dart
@@ -21,50 +21,76 @@ class FilterButton extends StatelessWidget {
     final activeStyle = theme.textTheme.body1.copyWith(
       color: theme.accentColor,
     );
+    final button = _Button(
+      onSelected: onSelected,
+      activeFilter: activeFilter,
+      activeStyle: activeStyle,
+      defaultStyle: defaultStyle,
+    );
 
     return AnimatedOpacity(
       opacity: isActive ? 1.0 : 0.0,
       duration: Duration(milliseconds: 150),
-      child: PopupMenuButton<VisibilityFilter>(
-        key: ArchSampleKeys.filterButton,
-        tooltip: ArchSampleLocalizations.of(context).filterTodos,
-        onSelected: onSelected,
-        itemBuilder: (BuildContext context) {
-          return <PopupMenuItem<VisibilityFilter>>[
-            PopupMenuItem<VisibilityFilter>(
-              key: ArchSampleKeys.allFilter,
-              value: VisibilityFilter.all,
-              child: Text(
-                ArchSampleLocalizations.of(context).showAll,
-                style: activeFilter == VisibilityFilter.all
-                    ? activeStyle
-                    : defaultStyle,
-              ),
+      child: isActive ? button : IgnorePointer(child: button),
+    );
+  }
+}
+
+class _Button extends StatelessWidget {
+  const _Button({
+    Key key,
+    @required this.onSelected,
+    @required this.activeFilter,
+    @required this.activeStyle,
+    @required this.defaultStyle,
+  }) : super(key: key);
+
+  final PopupMenuItemSelected<VisibilityFilter> onSelected;
+  final VisibilityFilter activeFilter;
+  final TextStyle activeStyle;
+  final TextStyle defaultStyle;
+
+  @override
+  Widget build(BuildContext context) {
+    return PopupMenuButton<VisibilityFilter>(
+      key: ArchSampleKeys.filterButton,
+      tooltip: ArchSampleLocalizations.of(context).filterTodos,
+      onSelected: onSelected,
+      itemBuilder: (BuildContext context) {
+        return <PopupMenuItem<VisibilityFilter>>[
+          PopupMenuItem<VisibilityFilter>(
+            key: ArchSampleKeys.allFilter,
+            value: VisibilityFilter.all,
+            child: Text(
+              ArchSampleLocalizations.of(context).showAll,
+              style: activeFilter == VisibilityFilter.all
+                  ? activeStyle
+                  : defaultStyle,
             ),
-            PopupMenuItem<VisibilityFilter>(
-              key: ArchSampleKeys.activeFilter,
-              value: VisibilityFilter.active,
-              child: Text(
-                ArchSampleLocalizations.of(context).showActive,
-                style: activeFilter == VisibilityFilter.active
-                    ? activeStyle
-                    : defaultStyle,
-              ),
+          ),
+          PopupMenuItem<VisibilityFilter>(
+            key: ArchSampleKeys.activeFilter,
+            value: VisibilityFilter.active,
+            child: Text(
+              ArchSampleLocalizations.of(context).showActive,
+              style: activeFilter == VisibilityFilter.active
+                  ? activeStyle
+                  : defaultStyle,
             ),
-            PopupMenuItem<VisibilityFilter>(
-              key: ArchSampleKeys.completedFilter,
-              value: VisibilityFilter.completed,
-              child: Text(
-                ArchSampleLocalizations.of(context).showCompleted,
-                style: activeFilter == VisibilityFilter.completed
-                    ? activeStyle
-                    : defaultStyle,
-              ),
+          ),
+          PopupMenuItem<VisibilityFilter>(
+            key: ArchSampleKeys.completedFilter,
+            value: VisibilityFilter.completed,
+            child: Text(
+              ArchSampleLocalizations.of(context).showCompleted,
+              style: activeFilter == VisibilityFilter.completed
+                  ? activeStyle
+                  : defaultStyle,
             ),
-          ];
-        },
-        icon: Icon(Icons.filter_list),
-      ),
+          ),
+        ];
+      },
+      icon: Icon(Icons.filter_list),
     );
   }
 }
