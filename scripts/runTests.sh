da7fbab3f6322fd4183ae70820fdc1ef1a500087
Maurice McCabe <mmcc007@users.noreply.github.com>
2018-09-25 11:00:44 -0700

    Workaround for built_redux build problem (#68)
    * Workaround for built_redux build problem
    Added codecov report
    
    * Fix pointer to codecov coverage report
    
    * upgrade flutter_built_redux to ^0.6.0
    
    * disabled coveralls reporting in favor of codecov
    
    * bypass codecov scanner for now
    (works on macos, not on linux)
    
    * Cleaned-up running dart tests

--- scripts/runTests.sh
+++ scripts/runTests.sh
@@ -7,14 +7,14 @@ runTests () {
   if [ -f "pubspec.yaml" ] && [ -d "test" ]; then
     echo "running tests in $1"
     # check if build_runner needs to be run
-    if grep build_runner pubspec.yaml > /dev/null; then
+    # todo: fix build in ./example/built_redux (not regenerating *.g.dart files in dart 2.0)
+    if grep build_runner pubspec.yaml > /dev/null  && [ "$1" != "./example/built_redux" ]; then
       flutter packages get
       flutter packages pub run build_runner build --delete-conflicting-outputs
     fi
     # run tests with coverage
     if grep flutter pubspec.yaml > /dev/null; then
       echo "run flutter tests"
-      rm -rf "coverage"
       if [ -f "test/all_tests.dart" ]; then
         flutter test --coverage test/all_tests.dart
       else
@@ -24,17 +24,16 @@ runTests () {
         # combine line coverage info from package tests to a common file
         escapedPath="$(echo ${1:2} | sed 's/\//\\\//g')"
         sed "s/^SF:lib/SF:$escapedPath\/lib/g" coverage/lcov.info >> $2/lcov.info
+        rm -rf "coverage"
       fi
     else
       # pure dart
       echo "run dart tests"
       pub get
       testFile="test/all_tests.dart"
-      echo $testFile
-      dart --preview-dart-2 --pause-isolates-on-exit --enable-vm-service=8111 $testFile &
-      sleep 5
-      pub global run coverage:collect_coverage --uri=http://localhost:8111 -o coverage.json --resume-isolates
-      pub global run coverage:format_coverage --packages=.packages -i coverage.json --report-on lib --lcov > lcov.info
+      pub global run coverage:collect_coverage --port=8111 -o coverage.json --resume-isolates --wait-paused &
+      dart --pause-isolates-on-exit --enable-vm-service=8111 $testFile
+      pub global run coverage:format_coverage --packages=.packages -i coverage.json --report-on lib --lcov --out lcov.info
       if [ -f "lcov.info" ]; then
         # combine line coverage info from package tests to a common file
         escapedPath="$(echo ${1:2} | sed 's/\//\\\//g')"
@@ -49,9 +48,7 @@ runTests () {
 export -f runTests
 
 # if running locally
-if [ -f "lcov.info" ]; then
-  rm lcov.info
-fi
+rm -f lcov.info
 
 # expects to find most packages at second directory level
 find . -maxdepth 2 -type d -exec bash -c 'runTests "$0" `pwd`' {} \;
