ede23a202f74287c6422fe6923207c896f4e2f2c
Maurice McCabe <mmcc007@gmail.com>
2019-05-24 20:49:29 -0700

    Improved output

--- scripts/android-wait-for-emulator.sh
+++ scripts/android-wait-for-emulator.sh
@@ -13,7 +13,8 @@ until [[ "$bootanim" =~ "stopped" ]]; do
   if [[ "$bootanim" =~ "device not found" || "$bootanim" =~ "device offline"
     || "$bootanim" =~ "running" ]]; then
     let "failcounter += 1"
-    echo "Waiting for emulator to start"
+    let "remaining = $timeout_in_sec - $failcounter"
+    echo "Waiting for emulator to start: $remaining of $timeout_in_sec : status: $bootanim"
     if [[ $failcounter -gt timeout_in_sec ]]; then
       echo "Timeout ($timeout_in_sec seconds) reached; failed to start emulator"
       exit 1
