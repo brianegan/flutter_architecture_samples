0d406b3c85d13948a0868d047293d350b6a17587
Kushagra Saxena <kkss420@gmail.com>
2019-08-20 00:05:50 +0530

    added the stats sreen

---
+++ mobx/lib/stats_counter.dart
@@ -0,0 +1,51 @@
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter_mobx/flutter_mobx.dart';
+import 'package:mobx_sample/model/todo_list.dart';
+import 'package:provider/provider.dart';
+
+class StatsCounter extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    TodoList todoList = Provider.of<TodoList>(context);
+    return Center(
+      child: Column(
+        mainAxisAlignment: MainAxisAlignment.center,
+        children: [
+          Padding(
+            padding: EdgeInsets.only(bottom: 8.0),
+            child: Text(
+              'Complete Todos',
+              style: Theme.of(context).textTheme.title,
+            ),
+          ),
+          Padding(
+            padding: EdgeInsets.only(bottom: 24.0),
+            child: Observer(
+              builder: (context) => Text(
+                todoList.completedTodos.length.toString(),
+                style: Theme.of(context).textTheme.subhead,
+              ),
+            ),
+          ),
+          Padding(
+            padding: EdgeInsets.only(bottom: 8.0),
+            child: Text(
+              'Active Todos',
+              style: Theme.of(context).textTheme.title,
+            ),
+          ),
+          Padding(
+            padding: EdgeInsets.only(bottom: 24.0),
+            child: Observer(
+              builder: (context) => Text(
+                todoList.pendingTodos.length.toString(),
+                style: Theme.of(context).textTheme.subhead,
+              ),
+            ),
+          )
+        ],
+      ),
+    );
+  }
+}
