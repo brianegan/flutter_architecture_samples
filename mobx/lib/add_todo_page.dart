207455fb000271dd09f9695ea716a345570e4e90
Kushagra Saxena <kkss420@gmail.com>
2019-08-20 00:16:23 +0530

    added routes

--- mobx/lib/add_todo_page.dart
+++ mobx/lib/add_todo_page.dart
@@ -3,6 +3,8 @@ import 'package:flutter_mobx/flutter_mobx.dart';
 import 'package:mobx_sample/model/todo.dart';
 
 class AddTodoPage extends StatefulWidget {
+  static const routeName = '/addTodo';
+
   final void Function(Todo) onAdd;
 
   AddTodoPage({@required this.onAdd});
