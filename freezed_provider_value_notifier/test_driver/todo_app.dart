45db324df2771777b5f823be48553215a424cbd2
Remi Rousselet <darky12s@gmail.com>
2020-02-10 15:05:06 +0000

    fix typo (freeze vs freezed)

---
+++ freezed_provider_value_notifier/test_driver/todo_app.dart
@@ -0,0 +1,13 @@
+// Copyright 2018 The Flutter Architecture Sample Authors. All rights reserved.
+// Use of this source code is governed by the MIT license that can be found
+// in the LICENSE file.
+
+// This line imports the extension
+import 'package:flutter_driver/driver_extension.dart';
+import 'package:freezed_provider_value_notifier/main.dart' as app;
+
+void main() {
+  enableFlutterDriverExtension();
+
+  app.main();
+}
