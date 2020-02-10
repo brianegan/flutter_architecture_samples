2c1739e9536d1f102b1ff39942376497dca27f73
Remi Rousselet <darky12s@gmail.com>
2020-02-10 11:38:23 +0000

    fork change_notifier_provider sample

---
+++ freeze_provider_value_notifier/lib/home/extra_actions_button.dart
@@ -0,0 +1,52 @@
+// Copyright 2018 The Flutter Architecture Sample Authors. All rights reserved.
+// Use of this source code is governed by the MIT license that can be found
+// in the LICENSE file.
+
+import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
+import 'package:todos_app_core/todos_app_core.dart';
+
+import '../todo_list_model.dart';
+
+class ExtraActionsButton extends StatelessWidget {
+  const ExtraActionsButton({
+    Key key,
+  }) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    final model = Provider.of<TodoListModel>(context);
+
+    return PopupMenuButton<ExtraAction>(
+      key: ArchSampleKeys.extraActionsButton,
+      onSelected: (action) {
+        switch (action) {
+          case ExtraAction.toggleAllComplete:
+            model.toggleAll();
+            break;
+          case ExtraAction.clearCompleted:
+            model.clearCompleted();
+            break;
+        }
+      },
+      itemBuilder: (BuildContext context) {
+        return <PopupMenuItem<ExtraAction>>[
+          PopupMenuItem<ExtraAction>(
+            key: ArchSampleKeys.toggleAll,
+            value: ExtraAction.toggleAllComplete,
+            child: Text(model.hasActiveTodos
+                ? ArchSampleLocalizations.of(context).markAllComplete
+                : ArchSampleLocalizations.of(context).markAllIncomplete),
+          ),
+          PopupMenuItem<ExtraAction>(
+            key: ArchSampleKeys.clearCompleted,
+            value: ExtraAction.clearCompleted,
+            child: Text(ArchSampleLocalizations.of(context).clearCompleted),
+          ),
+        ];
+      },
+    );
+  }
+}
+
+enum ExtraAction { toggleAllComplete, clearCompleted }
---
+++ freeze_provider_value_notifier/test_driver/todo_app_test.dart
@@ -0,0 +1,9 @@
+// _copyright 2018 _the _flutter _architecture _sample _authors. _all rights reserved.
+// _use of this source code is governed by the _m_i_t license that can be found
+// in the _l_i_c_e_n_s_e file.
+
+import 'package:integration_tests/integration_tests.dart' as integration_tests;
+
+void main() {
+  integration_tests.main();
+}
