8698493a665d60ba914b76c8b990306cc19ebb33
Brian Egan <brian@brianegan.com>
2025-07-06 14:58:17 +0100

    Remove deprecated samples

--- mvc/lib/main.dart
+++
@@ -1,7 +0,0 @@
-import 'package:flutter/material.dart';
-
-import 'package:mvc/src/App.dart';
-
-void main() {
-  runApp(MVCApp());
-}
--- mvu/test_driver/todo_app.dart
+++
@@ -1,8 +0,0 @@
-import 'package:flutter_driver/driver_extension.dart';
-import 'package:mvu/main.dart' as app;
-
-void main() {
-  enableFlutterDriverExtension();
-
-  app.main();
-}
